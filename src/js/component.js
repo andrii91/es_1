41d7ddb5d1aad93c5058cc5b447ebd09b3d03c47
Andrii <litvin.andriy91@gmail.com>
2021-04-20 15:31:47 +0300

    add pr

---
+++ src/js/component.js
@@ -0,0 +1,89 @@
+$(document).ready(function() { 
+$(window).scroll(function () {
+    return $('.nav').toggleClass("fixed", $(window).scrollTop() > 0);
+  });
+  
+  if($(window).scrollTop() > 0) {
+    $('.nav').addClass('fixed');
+  }
+
+  $('#nav-icon').click(function () {
+    $('.hamburger').toggleClass('is-active')
+    $('.nav').toggleClass('menu-open')
+
+    $(this).toggleClass('open');
+    $(this).parents('nav').toggleClass('open');
+    $('#menu').slideToggle(100);
+    $('header, main,footer').toggleClass('blur');
+    $('body').toggleClass('overflow-hidden')
+
+  });
+
+
+
+  $(window).resize(function () {
+    console.log($(this).width());
+    if ($(this).width() > 991) {
+      $('#menu').css({
+        'display': 'flex'
+      })
+      $('.nav').removeClass('open');
+      $('body *').removeClass('blur');
+
+      $('#nav-icon').css({
+        'display': 'none !important'
+      })
+
+      $('.home').removeClass('overflow-hidden')
+    } else {
+      if (!$('#nav-icon').hasClass('open')) {
+        $('#menu').css({
+          'display': 'none'
+        })
+        $('#nav-icon').css({
+          'display': 'block !important'
+        })
+      } else {
+        $('#menu').css({
+          'display': 'block'
+        })
+        $('.home').addClass('overflow-hidden');
+        $('header, main,footer').addClass('blur');
+      }
+    }
+  })
+
+
+
+  $('.header__item').click(function (e) {
+    const parent = $(e.currentTarget).parent();
+
+    if (parent.hasClass('expand__item')) {
+      e.preventDefault();
+
+      if (parent.hasClass('active')) {
+        parent.removeClass('active');
+        parent.mouseleave();
+      } else {
+        $('.expand__item').removeClass('active');
+        parent.addClass('active');
+      }
+
+      return;
+    }
+
+    if ($(window).width() <= 1023) toggleMenu()
+  })
+
+
+  function toggleMenu() {
+    $('.hamburger').toggleClass('is-active')
+    $('.nav').toggleClass('menu-open')
+
+    $(this).toggleClass('open');
+    $(this).parents('nav').toggleClass('open');
+    $('#menu').slideToggle(100);
+    $('header, main,footer').toggleClass('blur');
+    $('body').toggleClass('overflow-hidden');
+  }
+});
