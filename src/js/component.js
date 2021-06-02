7eeb61618ef33432bdafd7c185173548759b3764
Andrii <litvin.andriy91@gmail.com>
2021-06-02 15:55:53 +0300

    fix

--- src/js/component.js
+++ src/js/component.js
@@ -1,9 +1,9 @@
-$(document).ready(function() { 
-$(window).scroll(function () {
+$(document).ready(function () {
+  $(window).scroll(function () {
     return $('.nav').toggleClass("fixed", $(window).scrollTop() > 0);
   });
-  
-  if($(window).scrollTop() > 0) {
+
+  if ($(window).scrollTop() > 0) {
     $('.nav').addClass('fixed');
   }
 
@@ -23,7 +23,7 @@ $(window).scroll(function () {
 
   $(window).resize(function () {
     console.log($(this).width());
-    if ($(this).width() > 991) {
+    if ($(this).width() > 1023) {
       $('#menu').css({
         'display': 'flex'
       })
@@ -87,3 +87,94 @@ $(window).scroll(function () {
     $('body').toggleClass('overflow-hidden');
   }
 });
+
+
+$(document).ready(function () {
+  $('.footer-btn--more').click(function (e) {
+    e.preventDefault();
+
+    $(this).hide();
+    $('.more-tools').slideDown(0).addClass('active');
+
+    if ($(window).width() > 1030) {
+      $('.footer_new-politic ul li:last-child').css('margin-top', $('.more-tools').height() + 124);
+    }
+
+  })
+
+  $(window).resize(function () {
+    if ($(window).width() > 1030 && $('.more-tools').hasClass('active')) {
+      $('.footer_new-politic ul li:last-child').css('margin-top', $('.more-tools').height() + 124);
+    } else {
+      $('.footer_new-politic ul li:last-child').removeAttr('style');
+    }
+  })
+
+  $('.scroll').click(function (e) {
+    event.preventDefault();
+    var id = $(this).attr('href'),
+      top = $(id).offset().top;
+
+    $('body,html').animate({
+      scrollTop: top - 80
+    }, 1500);
+
+  });
+})
+
+$(document).ready(function () {
+  //ACCORDION
+  $('.accordion a').click(function (j) {
+    var dropDown = $(this).closest('li').find('.accordion__item-text')
+
+    $(this).closest('.accordion').find('.accordion__item-text').not(dropDown).slideUp()
+
+    if ($(this).hasClass('active')) {
+      $(this).removeClass('active')
+    } else {
+      $(this).closest('.accordion').find('a.active').removeClass('active')
+      $(this).addClass('active')
+    }
+
+    dropDown.stop(false, true).slideToggle(200)
+
+    j.preventDefault()
+  });
+
+  
+      $('.testimonials__slider').slick({
+        slidesToShow: 3, //3
+        slidesToScroll: 3, //2
+        infinite: false,
+        variableWidth: false,
+        swipe: false,
+        adaptiveHeight: true,
+        appendArrows: '.reviews__arrows',
+        prevArrow: '<div class="arrow arrow_prev"><svg xmlns="http://www.w3.org/2000/svg" width="21" height="12" viewBox="0 0 21 12" fill="none"><path d="M20.5303 6.53033C20.8232 6.23744 20.8232 5.76256 20.5303 5.46967L15.7574 0.696699C15.4645 0.403806 14.9896 0.403806 14.6967 0.696699C14.4038 0.989593 14.4038 1.46447 14.6967 1.75736L18.9393 6L14.6967 10.2426C14.4038 10.5355 14.4038 11.0104 14.6967 11.3033C14.9896 11.5962 15.4645 11.5962 15.7574 11.3033L20.5303 6.53033ZM0 6.75H20V5.25H0V6.75Z"fill="#505876"/></svg></div>',
+        nextArrow: '<div class="arrow arrow_next"><svg xmlns="http://www.w3.org/2000/svg" width="21" height="12" viewBox="0 0 21 12" fill="none"><path d="M20.5303 6.53033C20.8232 6.23744 20.8232 5.76256 20.5303 5.46967L15.7574 0.696699C15.4645 0.403806 14.9896 0.403806 14.6967 0.696699C14.4038 0.989593 14.4038 1.46447 14.6967 1.75736L18.9393 6L14.6967 10.2426C14.4038 10.5355 14.4038 11.0104 14.6967 11.3033C14.9896 11.5962 15.4645 11.5962 15.7574 11.3033L20.5303 6.53033ZM0 6.75H20V5.25H0V6.75Z"fill="#505876"/></svg></div>',
+        responsive: [
+            {
+                breakpoint: 1250,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                  adaptiveHeight: true,
+                }
+            },
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    dots: true,
+                    variableWidth: false,
+                    dotsClass: 'dots dots_grey',
+                    adaptiveHeight: true,
+                    swipe: true,
+                    arrows: false
+                }
+            }
+        ]
+    })
+  
+})
